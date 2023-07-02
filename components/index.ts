cf6b9fe661e89a85a4f1f22e1a4e494b2e6d6cd4
David Avila <davidavila6002@gmail.com>
2023-07-01 19:39:00 -0600

    first commit

---
+++ components/index.ts
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CardCard from "./CardCard";
+import CardDetails from "./CardDetails";
+
+export {
+   Hero,
+   CustomButton,
+   Navbar,
+   Footer,
+   SearchBar,
+   CustomFilter,
+   SearchManufacturer,
+   CardCard,
+   CardDetails,
+};
