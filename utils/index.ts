9fe265ea371207b2f9204d433805d0852dc1e6ec
David Avila <davidavila6002@gmail.com>
2023-07-05 19:17:12 -0600

    Filter is working

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,15 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+   const { manufacturer, year, model, limit, fuel } = filters;
+
    const headers = {
       "X-RapidAPI-Key": "b077e3a60bmshd7f0d9f767544d1p1e9802jsnb972f3b636dc",
       "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
    };
 
    const response = await fetch(
-      "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera",
+      `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
       { headers: headers }
    );
 
@@ -43,4 +45,14 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
    url.searchParams.append("angle", `${angle}`);
 
    return `${url}`;
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+   const searchParams = new URLSearchParams(window.location.search);
+
+   searchParams.set(type, value);
+
+   const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+   return newPathname;
 };
